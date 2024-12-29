e82a8d03f928681cb0267ddc78696b213646505c
Kia <kiasambrook@btinternet.com>
2024-12-29 13:39:05 +0000

    feat: add effects to swiper

--- frontend/src/components/Swiper.tsx
+++ frontend/src/components/Swiper.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectCards } from 'swiper/modules';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-
+import 'swiper/css/effect-cards';
 
 interface SwiperProps {
     children: React.ReactNode;
@@ -43,7 +43,7 @@ const SwiperFC = ({ children, ...props }: SwiperProps) => {
     });
 
     return (
-        <Swiper {...props} modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}>
+        <Swiper {...props} modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectCards]}>
             {slides}
         </Swiper>
     );
