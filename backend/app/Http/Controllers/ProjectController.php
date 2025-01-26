0113731c9f8e8cdd9d52f833fc2236b009a74552
Kia <kiasambrook@btinternet.com>
2025-01-26 13:16:16 +0000

    feat: setup project endpoint

--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function __invoke()
+    {
+        // Return all projects with assoicated skills
+        return Project::with('skills')->get();
+    }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    Route::get('/projects', [ProjectController::class, 'index']);
 });
+
+Route::get('/projects', ProjectController::class);
