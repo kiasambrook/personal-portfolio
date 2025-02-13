6d96a82a81513b6f537ac7b73138e9a144bb6882
Kia <22255229+kiasambrook@users.noreply.github.com>
2025-02-13 19:58:45 +0000

    feat: working project early live version

--- frontend/src/features/Timeline/CardStack.tsx
+++ frontend/src/features/Timeline/CardStack.tsx
@@ -3,29 +3,52 @@ import { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect, useState } from 'react';
+import ExperienceApi from "./api/service";
+import { Experience } from "@customTypes/experience";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function CardStack() {
+    const [experiences, setExperiences] = useState<Experience[]>([]);
+
+    useEffect(() => {
+        const fetchExperiences = async () => {
+            try {
+                const experienceService: ExperienceApi = new ExperienceApi();
+                setExperiences(await experienceService.fetchExperiences());
+            } catch (error) {
+                console.error('Error fetching experiences:', error);
+            }
+        }
+
+        fetchExperiences();
+    }, []);
+
+
     const cardContainer = useRef<HTMLDivElement>(null);
 
     useGSAP(() => {
+        // if experiences is empty do nothing
+        if (!experiences.length) return;
+
         const time = 100;
         const cards = gsap.utils.toArray("[data-card]");
 
         gsap.set(cards, {
-            y: (index) => 30 * index,
+            y: (index) => 10 * index,
             transformStyle: "preserve-3d", // For the perspecitve effect
         });
 
         const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: cardContainer.current,
-                start: "top 25%",
-                end: `${window.innerHeight * 5} top`,
-                scrub: true,
+                start: "top 15%", // Adjusted start property
+                end: "bottom bottom", // Adjusted end property to ensure pin waits until the end
+                scrub: 3, // Adjusted scrub property to slow down the scrolling
                 pin: "#timeline",
                 anticipatePin: 1,
+                markers: false,
             }
         });
 
@@ -46,23 +69,36 @@ export default function CardStack() {
             },
             time / 2
         );
-    }, []);
+    }, {
+        dependencies: [experiences],
+    }
+    );
 
+    const formatDate = (date: Date) => {
+        const formatDate = new Date(date);
+        return formatDate.toLocaleDateString('en-US', {
+            month: 'short',
+            year: 'numeric'
+        });
+    }
 
     return (
-        <div ref={cardContainer} className="grid justify-items-center">
-            <Card title="Software Engineer I" subTitle="Company Ltd." className="rotate-3 p-7 col-start-1 col-end-2 row-start-1 row-end-2 ">
-                <p className="font-semibold">August 2024 - present</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa asperiores, quaerat quis, repellendus dolor cupiditate alias voluptatem nesciunt voluptates, nemo fuga eos commodi accusamus reprehenderit ipsum laboriosam cumque est? A.</p>
-            </Card>
-            <Card title="Web Developer" subTitle="Company Ltd." className="-rotate-3 p-7 col-start-1 col-end-2 row-start-1 row-end-2 ">
-                <p className="font-semibold">August 2022 - August 2024</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa asperiores, quaerat quis, repellendus dolor cupiditate alias voluptatem nesciunt voluptates, nemo fuga eos commodi accusamus reprehenderit ipsum laboriosam cumque est? A.</p>
-            </Card>
-            <Card title="BSc Computer Science" subTitle="University" className="rotate-3 p-7 col-start-1 col-end-2 row-start-1 row-end-2 ">
-                <p className="font-semibold">1st Class Honors</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus doloribus cupiditate cum, praesentium laboriosam, voluptatem odit quos minus iusto dicta inventore perferendis. Perferendis pariatur optio explicabo doloremque, velit laudantium aspernatur!</p>
-            </Card>
+        <div ref={cardContainer} className="grid justify-items-center h-full">
+            {experiences.map((experience, index) => {
+                const rotationClasses = index % 2 === 0 ? "rotate-3" : "-rotate-3";
+                return (
+                    <Card
+                        key={index}
+                        title={experience.title}
+                        subTitle={experience.subtitle}
+                        className={`${rotationClasses} p-7 col-start-1 col-end-2 row-start-1 row-end-2 h-fit`}
+                    >
+                        <p className="font-semibold">{formatDate(experience.start_date)}</p>
+                        <p>{experience.description}</p>
+                    </Card>
+                )
+            })
+            }
         </div>
     )
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), chunkSplitPlugin({
-    strategy: 'single-vendor',
-    customChunk: (args) => {
-      let { file, id, moduleId, root } = args;
-      if (file.startsWith('src/pages/')) {
-        file = file.substring(4);
-        file = file.replace(/\.[^.$]+$/, '');
-        return file;
-      }
-      return null;
-    },
-    customSplitting: {
-      'react-vendor': [/react/, /react-dom/],
-      'utils': [/src\/utils/]
-    }
-  })],
+  plugins: [react(), 
+  //   chunkSplitPlugin({
+  //   strategy: 'single-vendor',
+  //   customChunk: (args) => {
+  //     let { file, id, moduleId, root } = args;
+  //     if (file.startsWith('src/pages/')) {
+  //       file = file.substring(4);
+  //       file = file.replace(/\.[^.$]+$/, '');
+  //       return file;
+  //     }
+  //     return null;
+  //   },
+  //   customSplitting: {
+  //     'react-vendor': [/react/, /react-dom/],
+  //     'utils': [/src\/utils/]
+  //   }
+  // })
+],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
