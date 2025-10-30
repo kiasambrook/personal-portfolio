8467e4867b4655471be4f57362f1b57dbd145c5e
Kia <22255229+kiasambrook@users.noreply.github.com>
2025-10-30 09:48:42 +0000

    remove welcome screen

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,10 +5,7 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\SkillController;
 use App\Http\Controllers\SocialLinkController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use Livewire\Livewire;
 
 Route::get('/projects', ProjectController::class);
 Route::get('/experience', ExperienceController::class);
