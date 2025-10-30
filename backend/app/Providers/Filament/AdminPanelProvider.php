43e914284991194b209ae0a3e1a108c541c0c07d
Kia <22255229+kiasambrook@users.noreply.github.com>
2025-10-30 09:59:35 +0000

    Update domain URL

--- backend/app/Providers/Filament/AdminPanelProvider.php
+++ backend/app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('/')
+            ->domain('admin.kiasambrook.co.uk')
             ->login()
             ->passwordReset()
             ->emailVerification()
