57ffb57aefb49a4c04d38aae7b340dce1702d34c
Kia <22255229+kiasambrook@users.noreply.github.com>
2025-11-01 15:30:25 +0000

    chore: improve icon fallback and removing unneeded comments

--- frontend/src/utils/iconMapping.ts
+++ frontend/src/utils/iconMapping.ts
@@ -3,9 +3,8 @@ import * as regularIcons from '@fortawesome/free-regular-svg-icons';
 import * as brandsIcons from '@fortawesome/free-brands-svg-icons';
 
 export const getIcon = (iconStr: string) => {
-  const [prefix, name] = iconStr.split('-'); // e.g. "fab-react" → ["fab", "react"]
-  const camelName = `fa${name.charAt(0).toUpperCase()}${name.slice(1)}`; // "react" → "faReact"
-
+  const [prefix, name] = iconStr.split('-');
+  const camelName = `fa${name.charAt(0).toUpperCase()}${name.slice(1)}`;
   switch (prefix) {
     case 'fas':
       return solidIcons[camelName];
@@ -14,6 +13,6 @@ export const getIcon = (iconStr: string) => {
     case 'fab':
       return brandsIcons[camelName];
     default:
-      return null;
+      return solidIcons.faCode;
   }
 };
